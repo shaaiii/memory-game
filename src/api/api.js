b6a8f36ae4880c4e4bf4fa83ead3b0864d76ede3
shaina <sdelossantos@loopstud.io>
2019-10-10 10:13:32 -0300

    json cards

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,209 @@
-//datos que tienen pi
+
+function getCards() {
+    return new Promise((resolve, reject) => {
+        resolve(cards)
+    })
+}
+const Cards = [
+    {
+        foto: null,
+        texto: "Polinomio",
+        idPar: 1,
+        estado: "oculta",
+        id: 1,
+    },
+    {
+        foto: null,
+        texto: "Q(x)",
+        idPar: 1,
+        estado: "oculta",
+        id: 2,
+
+    }, {
+        foto: null,
+        texto: "Ruffini",
+        idPar: 2,
+        estado: "oculta",
+        id: 3,
+    },
+    {
+        foto: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiLqIv8n4DlAhWVA9QKHaFXBWgQjRx6BAgBEAQ&url=https%3A%2F%2Fekuatio.com%2Fla-regla-de-ruffini%2F&psig=AOvVaw3Qt_jClIdS9-oJWS8OJKsr&ust=1570197577791581",
+        texto: null,
+        idPar: 2,
+        estado: "oculta",
+        id: 4,
+
+    }, {
+        foto: null,
+        texto: "Funcion Racional",
+        idPar: 3,
+        estado: "oculta",
+        id: 5,
+    },
+    {
+        foto: null,
+        texto: "f(x)=P(x)/Q(x)",
+        idPar: 3,
+        estado: "oculta",
+        id: 6,
+
+    }, {
+        foto:null,
+        texto: "Funcion exponencial",
+        idPar: 4,
+        estado: "oculta",
+        id: 7,
+    },
+    {
+        foto: "https://sites.google.com/site/portafoliodeerickvillaseca/_/rsrc/1468755338259/funciones-exponenciales/funcion-exponencial-natural/grafica%20de%20e%5Ex.png?height=299&width=320",
+        texto: null,
+        idPar: 4,
+        estado: "oculta",
+        id: 8,
+
+    }, {
+        foto: "",
+        texto: "Programación lineal",
+        idPar: 5,
+        estado: "oculta",
+        id: 9,
+    },
+    {
+        foto: "https://www.gestiopolis.com/wp-content/uploads/2013/10/programacion-lineal-en-la-investigacion-de-operaciones1.gif",
+        texto: null,
+        idPar: 5,
+        estado: "oculta",
+        id: 10,
+
+    }, {
+        foto: null,
+        texto: "Inecuaciones",
+        idPar: 6,
+        estado: "oculta",
+        id: 11,
+    },
+    {
+        foto: null,
+        texto: "x+1>10",
+        idPar: 6,
+        estado: "oculta",
+        id: 12,
+
+    }, {
+        foto: null,
+        texto: "Potencias",
+        idPar: 7,
+        estado: "oculta",
+        id: 13,
+    },
+    {
+        foto:null,
+        texto: "2^3",
+        idPar: 7,
+        estado: "oculta",
+        id: 14,
+
+    }, {
+        foto: null,
+        texto: "Funcion logaritmica",
+        idPar: 8,
+        estado: "oculta",
+        id: 15,
+    },
+    {
+        foto: "https://www.varsitytutors.com/assets/vt-hotmath-legacy/hotmath_help/spanish/topics/logarithmic-functions/log-function.gif",
+        texto: null,
+        idPar: 8,
+        estado: "oculta",
+        id: 16,
+
+    }, {
+        foto: null,
+        texto: "Ecuaciones",
+        idPar: 9,
+        estado: "oculta",
+        id: 17,
+    },
+    {
+        foto: null,
+        texto: "x+2=7",
+        idPar: 9,
+        estado: "oculta",
+        id: 18,
+
+    }, {
+        foto: null,
+        texto: "Bhaskara",
+        idPar: 10,
+        estado: "oculta",
+        id: 19,
+    },
+    {
+        foto: "https://s2.static.brasilescola.uol.com.br/img/2016/04/formula-de-bhaskara.jpg",
+        texto: null,
+        idPar: 10,
+        estado: "oculta",
+        id: 20,
+
+    }, {
+        foto: null,
+        texto: "Conjuntos",
+        idPar: 11,
+        estado: "oculta",
+        id: 21,
+    },
+    {
+        foto: "https://www.disfrutalasmatematicas.com/conjuntos/images/function-mapping.gif",
+        texto: null,
+        idPar: 11,
+        estado: "oculta",
+        id: 22,
+
+    }, {
+        foto: null,
+        texto: "Sumatoria",
+        idPar: 12,
+        estado: "oculta",
+        id: 23,
+    },
+    {
+        foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Greek_uc_sigma.svg/1200px-Greek_uc_sigma.svg.png",
+        texto: null,
+        idPar: 12,
+        estado: "oculta",
+        id: 24,
+
+    }, {
+        foto: null,
+        texto: "Dominio",
+        idPar: 13,
+        estado: "oculta",
+        id: 25,
+    },
+    {
+        foto: null,
+        texto: "Condominio",
+        idPar: 13,
+        estado: "oculta",
+        id: 26,
+
+    }, {
+        foto: null,
+        texto: "Simbolo de mayor e igual",
+        idPar: 14,
+        estado: "oculta",
+        id: 27,
+    },
+    {
+        foto: "https://image.flaticon.com/icons/png/512/43/43716.png",
+        texto: null,
+        idPar: 14,
+        estado: "oculta",
+        id: 28,
+
+    }
+
+];
+export {
+    getCards
+}
